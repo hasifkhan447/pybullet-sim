481ed668fcc63b3a8d0193d979095bc9053bb1d6
Hamad Khan <hasifkhan.447@gmail.com>
2025-06-05 08:14:12 +0500

    Seperated plotting logic

--- trajectory.py
+++ trajectory.py
@@ -5,7 +5,7 @@ import pybullet_data
 import numpy as np
 from scipy.interpolate import make_interp_spline
 import time
-import matplotlib.pyplot as plt
+from plotting import plot_system
 
 from scipy.signal import butter, filtfilt
 
@@ -172,40 +172,8 @@ for pos in trajectory:
 positions = np.array([[s[i][0] for i in range(len(s))] for s in states])  # shape (timesteps, joints)
 velocities = np.array([[s[i][1] for i in range(len(s))] for s in states])
 torques = np.array([[s[i][3] for i in range(len(s))] for s in states])
-
-
 time_axis = np.arange(len(positions)) * dt
-
-plt.figure(figsize=(10, 8))
-plt.subplot(5, 1, 1)
-for joint_id in range(num_joints):
-        plt.plot(time_axis, positions[:, joint_id], label=f"joint {joint_id}")
-plt.legend(loc='center left',bbox_to_anchor=(1,0.05))
-plt.grid(True)
-plt.title(f'Joint Positions')
-
-plt.subplot(5, 1, 2)
-for joint_id in range(num_joints):
-        plt.plot(time_axis, velocities[:, joint_id], label=f"joint {joint_id}")
-        plt.title(f'Joint {joint_id} Velocity')
-plt.legend(loc='center left',bbox_to_anchor=(1,0.05))
-plt.grid(True)
-plt.title(f'Joint Velocities')
-
-plt.subplot(5, 1, 3)
-for joint_id in range(num_joints):
-        accelerations = np.gradient(velocities, dt, axis=0)
-        plt.plot(time_axis, accelerations[:, joint_id], label=f"joint {joint_id}")
-plt.legend(loc='center left',bbox_to_anchor=(1,0.05))
-plt.grid(True)
-plt.title(f'Joint Accelerations')
-
-plt.subplot(5, 1, 4)
-for joint_id in range(num_joints):
-        plt.plot(time_axis, torques[:, joint_id], label=f"joint {joint_id}")
-plt.title(f'Joint Torques')
-plt.legend(loc='center left',bbox_to_anchor=(1,0.05))
-plt.grid(True)
+accelerations = np.gradient(velocities, dt, axis=0)
 
 motor_torques = []
 for i in range(len(positions)):
@@ -217,30 +185,6 @@ for i in range(len(positions)):
 
 motor_torques = np.array(motor_torques)
 
-# --- Plot Motor Torques ---
-plt.subplot(5, 1, 5)
-for joint_id in range(num_joints):
-    plt.plot(time_axis, motor_torques[:, joint_id], label=f"joint {joint_id}")
-plt.title("Motor Torques (Inverse Dynamics)")
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.05))
-plt.grid(True)
 
 
-
-plt.tight_layout()
-plt.show()
-
-
-# --- Torque Statistics ---
-for joint_id in range(num_joints):
-    joint_torques = motor_torques[:, joint_id]
-    avg_torque = np.mean(np.abs(joint_torques))
-    max_torque = np.max(np.abs(joint_torques))
-
-    joint_velocities = velocities[:, joint_id]
-    avg_velocity = np.mean(np.abs(joint_velocities))
-    max_velocity = np.max(np.abs(joint_velocities))
-
-    print(f"Joint {joint_id}:")
-    print(f"  Avg Torque     = {avg_torque:.4f}  Avg velocity   = {avg_velocity:.4f}")
-    print(f"  Max Torque     = {max_torque:.4f}  Max Velocity   = {max_velocity:.4f}")
+plot_system(positions, velocities, accelerations, torques, motor_torques, time_axis, dt, num_joints)
