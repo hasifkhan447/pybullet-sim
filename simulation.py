550eb10028c460dbb7b434c82118f6d078812099
Hamad Khan <hasifkhan.447@gmail.com>
2025-06-12 11:01:13 +0500

    Added the rest of the environment

--- simulation.py
+++ simulation.py
@@ -11,47 +11,60 @@ xml = """
     </option>
     <default>
         <joint type="hinge" axis="0 -1 0" limited="true" range="-1.14 1.14"/>
-        <geom type="cylinder" size=".06"/>
+        <geom type="capsule" size=".06"/>
     </default>
     <worldbody>
-    <body name="arm" pos="0 0 .5">
+    <body name="arm" pos="0.1 -.6 .4">
         <joint name="root" type="hinge" axis="0 0 1" limited="true" range="-3.14 1.14"/>
-            <geom fromto="0 0 0 0 0 .3" rgba="1 1 0 1"/>
+            <geom fromto="0 0 0 0 0 .3" rgba="1 0.5 0 1"/>
         <body name="1" pos="0 0 .3">
             <joint name="1"/>
-            <geom fromto="0 0 .01 0 0 .3" rgba="1 0 0 1"/>
+            <geom fromto="0 0 .05 0 0 .3" rgba="0.5 0 0 1"/>
 
 
             <body name="2" pos="0 0 .3">
                 <joint name="2"/>
-                <geom fromto="0 0 .01 0 0 .3" rgba="0 1 0 1"/>
+                <geom fromto="0 0 .05 0 0 .3" rgba="0 0.5 0 1"/>
 
                 <body name="3" pos="0 0 .3">
                     <joint name="3"/>
-                    <geom fromto="0 0 .01 0 0 .3" rgba="0 0 1 1"/>
+                    <geom fromto="0 0 .05 0 0 .3" rgba="0 0 0.5 1"/>
                 </body>
             </body>
         </body>
     </body>
 
-
     <body name="microwave" pos="0 0 0">
-        <geom type='box' size='.3556 .4318 .2794' pos='0.5 0 0.5' rgba="1 1 0 1"/>
+        <geom type='box' size='.176 .215 .137' pos='0.5 0 0.5' rgba="1 1 1 1"/>
     </body>
+
+
+    <body name="pallet" pos="0 0 0">
+        <geom type='box' size='.5 .5 .05' pos='-0.5 0 0' rgba="1 1 0 1"/>
+    </body>
+
+
 
 
     </worldbody>
 
 
     <actuator>
-      <motor joint="root" gear="100"/>
-      <motor joint="1"    gear="100"/>
-      <motor joint="2"    gear="100"/>
-      <motor joint="3"    gear="100"/>
+      <motor joint="root" gear="10000"/>
+      <motor joint="1"    gear="10000"/>
+      <motor joint="2"    gear="10000"/>
+      <motor joint="3"    gear="10000"/>
     </actuator>
 
 </mujoco>
 """
+
+
+pick_state="-2.1611 0.680977 1.15222 0.294862"
+
+place_state="-0.581378 0.856388 1.09171 0.767273"
+
+
 
 model = m.MjModel.from_xml_string(xml)
 data = m.MjData(model)
